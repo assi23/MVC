76b79b1d4413897ab50f7ec8a727f5c2c43464e1
assi23 <guiassi@hotmail.com>
2021-08-06 16:28:43 -0300

    add controller genérico

---
+++ WebMVC2/WebMVC2/WebMVC2/Controllers/BaseController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebMVC2.Models;
+using WebMVC2.Repository;
+
+namespace WebMVC2.Controllers
+{
+    public class BaseController<M,R> : Controller where M : BaseModel where R : BaseRepository<M>  
+    {
+
+        R repository;
+		public BaseController(R repository)
+		{
+            this.repository = repository;
+		}
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(M model)
+        {
+            repository.Create(model);
+            ModelState.Clear();
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            M model = repository.Read(id);
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Edit(M model)
+        {
+            repository.Update(model);
+            return RedirectToAction("List");
+        }
+        public ActionResult List()
+        {
+            return View(repository.Read());
+        }
+        public ActionResult Details(int id)
+        {
+            return View(repository.Read(id));
+        }
+        public ActionResult Delete(int id)
+        {
+            repository.Delete(id);
+            return RedirectToAction("List");
+        }
+        public ActionResult DeletePage(int id)
+        {
+            return View(repository.Read(id));
+        }
+    }
+}
--- WebMVC2/WebMVC2/WebMVC2/Controllers/ProdutoController.cs
+++ WebMVC2/WebMVC2/WebMVC2/Controllers/ProdutoController.cs
@@ -8,53 +8,11 @@ using WebMVC2.Repository;
 
 namespace WebMVC2.Controllers
 {
-    public class ProdutoController : Controller
+    public class ProdutoController : BaseController<Produto,ProdutoRepository>
     {
-        ProdutoRepository repository = new ProdutoRepository();
-        public ActionResult Index()
-        {
-            return View();
-        }
-        [HttpGet]
-        public ActionResult Create() 
-        {
-            return View();
-        }
-        [HttpPost]
-        public ActionResult Create(Produto model)
-        {
-            repository.Create(model);
-            ModelState.Clear();
-            return View();
-        }
-        [HttpGet]
-        public ActionResult Edit(int id)
-        {
-            Produto model = repository.Read(id);
-            return View(model);
-        }
-        [HttpPost]
-        public ActionResult Edit(Produto model)
-        {
-            repository.Update(model);
-            return RedirectToAction("List");
-        }
-        public ActionResult List()
-        {
-            return View(repository.Read());
-        }
-        public ActionResult Details(int id) 
-        {
-            return View(repository.Read(id));
-        }
-        public ActionResult Delete(int id) 
-        {
-            repository.Delete(id);
-            return RedirectToAction("List");
-        }
-        public ActionResult DeletePage(int id)
-        {
-            return View(repository.Read(id));
-        }
+		public ProdutoController():base(new ProdutoRepository())
+		{
+
+		}
     }
 }
